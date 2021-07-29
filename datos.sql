ba78a33b707eef6a4f9df4e40199fb6166640d3a
Jorge Gonzalez <jegnzc@gmail.com>
2021-07-28 21:58:37 -0600

    Agregados datos para probar la base de datos

---
+++ datos.sql
@@ -0,0 +1,15 @@
+select * from jugador;
+select * from equipo;
+select * from deporte;
+select * from rol;
+INSERT INTO deporte(pkId, nombre) VALUES (NULL, "Fútbol");
+INSERT INTO rol(pkId, nombre, fkIdDeporte) VALUES (NULL, "Delantero", 1);
+INSERT INTO equipo(pkId, nombre, fkIdDeporte) VALUES (NULL, "Barcelona", "1");
+INSERT INTO jugador(pkId, nombre, anotaciones, fotografia, fkIdEquipo, fkIdRol) VALUES (NULL, "Melanie Revolorio", "90", NULL, 1, 1);
+INSERT INTO jugador(pkId, nombre, anotaciones, fotografia, fkIdEquipo, fkIdRol) VALUES (NULL, "Jorge González", "50", NULL, 1, 1);
+
+CREATE VIEW `vwJugador` AS
+SELECT A.pkId AS pkIdJugador, A.nombre, A.anotaciones, C.pkId AS pkIdRol, C.nombre AS rol, B.pkId AS pkIdEquipo, B.nombre AS equipo
+FROM jugador A
+LEFT JOIN equipo B ON B.pkId = A.fkIdEquipo
+INNER JOIN rol C ON  C.pkId = A.fkIdRol;

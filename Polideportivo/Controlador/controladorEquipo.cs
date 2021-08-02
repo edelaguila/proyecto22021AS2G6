c9a2263c7f6bc0f7c280732b65db198f0fb9fc32
Roberto <robicarlomann@hotmail.com>
2021-08-01 22:38:58 -0600

    Cambio a polideportivo, controlador equipo y modelo

--- Polideportivo/Controlador/controladorEquipo.cs
+++ Polideportivo/Controlador/controladorEquipo.cs
@@ -18,11 +18,62 @@ namespace Polideportivo.Controlador
 
 
         }
+
         public controladorEquipo(modeloEquipo Modelo)
         {
             modelo = Modelo;
         }
         OdbcConnection conexionOdbc = new OdbcConnection("DSN=bdpolideportivo");
+
+        public modeloEquipo agregarEquipo(modeloEquipo modelo)
+        {
+
+            var sqlinsertar =
+                "INSERT INTO equipo (pkId, nombre, fkIdEntrenador, fkIdDeporte) " +
+                "VALUES (NULL, ?nombre?, ?fkIdEntrenador?, ?fkIdDeporte?);";
+            var ValorDeVariables = new
+            {
+                nombre = modelo.nombre,
+                fkIdEntrenador = modelo.fkIdEntrenador,
+                fkIdDeporte = modelo.fkIdDeporte
+            };
+            var resultadoinsertar = conexionOdbc.Execute(sqlinsertar, ValorDeVariables);
+
+
+            conexionOdbc.Close();
+
+            //modelo.id = 5;
+            return modelo;
+        }
+        public modeloEquipo modificarEquipo(modeloEquipo modelo)
+        {
+
+            var sqlinsertar =
+                "UPDATE jugador SET nombre = ?nombre?, anotaciones = ?anotaciones?, " +
+                "fkIdEquipo = ?fkIdEquipo?, fkIdRol = ?fkIdRol?, fkIdDeporte = ?fkIdDeporte? " +
+                "WHERE pkId = ?pkId?;";
+            var ValorDeVariables = new
+            {
+                nombre = modelo.nombre,
+                fkIdEntrenador = modelo.fkIdEntrenador,
+                fkIdDeporte = modelo.fkIdDeporte,
+                pkId = modelo.pkId
+            };
+            var resultadoinsertar = conexionOdbc.Execute(sqlinsertar, ValorDeVariables);
+            conexionOdbc.Close();
+            return modelo;
+        }
+        public IEnumerable<modeloEquipo> mostrarEquipos()
+        {
+            IEnumerable<modeloEquipo> modeloList = new List<modeloEquipo>();
+            conexionOdbc.Open();
+            string sqlconsulta = "SELECT * FROM tablajugadores;";
+            var sqlresultado = conexionOdbc.Query<modeloEquipo>(sqlconsulta);
+            modeloList = sqlresultado;
+            conexionOdbc.Close();
+            return modeloList;
+        }
+
         public List<modeloEquipo> mostrarEquipoPorDeporte()
         {
             conexionOdbc.Open();
@@ -35,5 +86,6 @@ namespace Polideportivo.Controlador
             conexionOdbc.Close();
             return sqlresultado;
         }
+
     }
 }

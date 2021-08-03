4e449ccf22c52c73f66bd92c65c5e4c3909da27e
Roberto <robicarlomann@hotmail.com>
2021-08-02 19:04:45 -0600

    Modelo partido y controlador

---
+++ Polideportivo/Controlador/controladorPartido.cs
@@ -0,0 +1,101 @@
+﻿using Polideportivo.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.Odbc;
+using System.Data;
+
+namespace Polideportivo.AccesoDatos
+{
+    class controladorPatido
+    {
+        OdbcConnection pruebas = new OdbcConnection("DSN=bdpolideportivo");
+        public modeloJugador agregarJugador(modeloJugador modelo)
+        {
+
+            var sqlinsertar =
+                "INSERT INTO jugador (pkId, equipo1, equipo2, campo, fecha, estado, fase, fkIdCampeonato, fkIdEmpleado, fkIdResultado) " +
+                "VALUES (NULL, ?equipo1?, ?equipo2?, ?campo??fkId?, ?fkIdRol?, ?fkIdDeporte?);";
+            var ValorDeVariables = new
+            {
+                nombre = modelo.nombre,
+                anotaciones = modelo.anotaciones,
+                fkIdEquipo = modelo.fkIdEquipo,
+                fkIdRol = modelo.fkIdRol,
+                fkIdDeporte = modelo.fkIdDeporte
+            };
+            var resultadoinsertar = pruebas.Execute(sqlinsertar, ValorDeVariables);
+
+
+            pruebas.Close();
+
+            //modelo.id = 5;
+            return modelo;
+        }
+
+        public modeloJugador modificarJugador(modeloJugador modelo)
+        {
+
+            var sqlinsertar =
+                "UPDATE jugador SET nombre = ?nombre?, anotaciones = ?anotaciones?, " +
+                "fkIdEquipo = ?fkIdEquipo?, fkIdRol = ?fkIdRol?, fkIdDeporte = ?fkIdDeporte? " +
+                "WHERE pkId = ?pkId?;";
+            var ValorDeVariables = new
+            {
+                nombre = modelo.nombre,
+                anotaciones = modelo.anotaciones,
+                fkIdEquipo = modelo.fkIdEquipo,
+                fkIdRol = modelo.fkIdRol,
+                fkIdDeporte = modelo.fkIdDeporte,
+                pkId = modelo.pkId
+            };
+            var resultadoinsertar = pruebas.Execute(sqlinsertar, ValorDeVariables);
+            pruebas.Close();
+            return modelo;
+        }
+
+        public IEnumerable<modeloJugador> mostrarJugadores()
+        {
+            IEnumerable<modeloJugador> modeloList = new List<modeloJugador>();
+            pruebas.Open();
+            string sqlconsulta = "SELECT * FROM tablajugadores;";
+            var sqlresultado = pruebas.Query<modeloJugador>(sqlconsulta);
+            modeloList = sqlresultado;
+            pruebas.Close();
+            return modeloList;
+        }
+
+
+
+        public IEnumerable<modeloJugador> mostrarJugadoresPorDeporte()
+        {
+            IEnumerable<modeloJugador> modeloList = new List<modeloJugador>();
+            pruebas.Open();
+            string sqlconsulta = "SELECT * FROM tablajugadores;";
+            var sqlresultado = pruebas.Query<modeloJugador>(sqlconsulta);
+            modeloList = sqlresultado;
+            pruebas.Close();
+            return modeloList;
+        }
+
+        public modeloJugador eliminarJugador(modeloJugador modelo)
+        {
+
+            var sqlinsertar =
+                "DELETE FROM jugador WHERE pkId = ?pkId?;";
+            var ValorDeVariables = new
+            {
+                pkId = modelo.pkId
+            };
+            var resultadoinsertar = pruebas.Execute(sqlinsertar, ValorDeVariables);
+            pruebas.Close();
+            return modelo;
+        }
+
+
+
+    }
+}
---
+++ Polideportivo/Modelo/modeloPartido.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polideportivo.Modelo
+{
+    public class modeloPartido
+    {
+        public int pkId { get; set; }
+        public string equipo1 { get; set; }
+        public string equipo2 { get; set; }
+        public string campo { get; set; }
+        public DateTime  fecha{ get; set; }
+        public string estado { get; set; }
+        public string fase { get; set; }
+        public int fkIdCampeonato { get; set; }
+        public int fkIdEmpleado { get; set; }
+        public int fkIdResultado { get; set; }
+
+        public modeloPartido() { }
+
+        public modeloPartido(int Id, string Equipo1, string Equipo2, string Campo, DateTime Fecha, string Estado, string Fase, int IdCampeonato, int IdEmpleado, int IdResultado)
+        {
+            pkId = Id;
+            equipo1 = Equipo1;
+            equipo2 = Equipo2;
+            campo = Campo;
+            fecha = Fecha;
+            estado = Estado;
+            fase = Fase;
+            fkIdCampeonato = IdCampeonato;
+            fkIdEmpleado = IdEmpleado;
+            fkIdResultado = IdResultado;
+            //para cuando sean ints
+            //int numeroValor = 0;
+            //int.TryParse(numero, out numeroValor);
+            //Numero = numeroValor;
+        }
+    }
+}

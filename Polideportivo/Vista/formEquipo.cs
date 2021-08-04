8a30e0d0720c8b8269c1edf009713a57d4281c31
Jorge Gonzalez <jegnzc@gmail.com>
2021-08-04 17:15:42 -0600

    Mejoras al código de equipo

--- Polideportivo/Vista/formEquipo.cs
+++ Polideportivo/Vista/formEquipo.cs
@@ -9,55 +9,21 @@ namespace Polideportivo.Vista
 {
     public partial class formEquipo : Form
     {
-        int id;
-        string nombre;
-        int fkIdDeporte;
 
-
+        controladorEquipo controladorEquipo = new controladorEquipo();
+        modeloEquipo modeloFila = new modeloEquipo();
 
         public formEquipo()
         {
             InitializeComponent();
         }
 
-
-
-
         private void formEquipo_Load(object sender, EventArgs e)
         {
-            // TODO: esta línea de código carga datos en la tabla 'vwEquipo.vwequipo' Puede moverla o quitarla según sea necesario.
-            this.vwequipoTableAdapter.Fill(this.vwEquipo.vwequipo);
-            // TODO: esta línea de código carga datos en la tabla 'vwEquipo.vwequipo' Puede moverla o quitarla según sea necesario.
-            //this.vwequipoTableAdapter.Fill(this.vwEquipo.vwequipo);
-
+            actualizarTablaJugadores();
         }
 
-
-        public void actualizarTablaJugadores()
-        {
-            this.vwequipoTableAdapter.Fill(this.vwEquipo.vwequipo);
-        }
-
-
-        private void txtJugadorFiltrar_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        modeloEquipo modeloFila = new modeloEquipo();
-
-        public void llenarModeloConFilaSeleccionada()
-        {
-            id = stringAInt(tablaEquipo.SelectedRows[0].Cells[0].Value.ToString());
-            nombre = tablaEquipo.SelectedRows[0].Cells[1].Value.ToString();
-            fkIdDeporte = stringAInt(tablaEquipo.SelectedRows[0].Cells[2].Value.ToString());
-            modeloFila.pkId = id;
-            modeloFila.nombre = nombre;
-            modeloFila.fkIdDeporte = fkIdDeporte;
-
-        }
-            
-            private void tablaJugadores_CellClick(object sender, DataGridViewCellEventArgs e)
+        private void tablaJugadores_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             llenarModeloConFilaSeleccionada();
         }
@@ -72,17 +38,11 @@ namespace Polideportivo.Vista
             abrirForm(new formEquipoEventos(modeloFila, this));
         }
 
-        private void txtFiltrar_TextChanged(object sender, EventArgs e)
+        private void btnEliminarEquipo_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(txtFiltrar.Text))
-            {
-                vwequipoBindingSource.Filter = string.Empty;
-                //.Filter = string.Empty;
-            }
-            else
-            {
-                vwequipoBindingSource.Filter = string.Format("{0}='{1}'", cboBuscar.Text, txtFiltrar.Text);
-            }
+            llenarModeloConFilaSeleccionada();
+            controladorEquipo.eliminarEquipo(modeloFila);
+            actualizarTablaJugadores();
         }
 
         private void cboBuscar_SelectedIndexChanged(object sender, EventArgs e)
@@ -95,28 +55,38 @@ namespace Polideportivo.Vista
             actualizarTablaJugadores();
         }
 
-        private void btnEliminarEquipo_Click(object sender, EventArgs e)
+        private void txtFiltrar_TextChanged(object sender, EventArgs e)
         {
-            llenarModeloConFilaSeleccionada();
-            controladorEquipo controlador = new controladorEquipo();
-           // controlador.eliminarEquipo(modeloFinal);
-            actualizarTablaJugadores();
+            filtrarTabla();
+        }
+
+        public void llenarModeloConFilaSeleccionada()
+        {
+            modeloFila.pkId = stringAInt(tablaEquipo.SelectedRows[0].Cells[0].Value.ToString());
+            modeloFila.nombre = tablaEquipo.SelectedRows[0].Cells[1].Value.ToString();
+            modeloFila.fkIdDeporte = stringAInt(tablaEquipo.SelectedRows[0].Cells[2].Value.ToString());
         }
 
         private void tablaEquipo_DataError(object sender, DataGridViewDataErrorEventArgs e)
         {
             // Es necesario para que no den errores cuando se cambia rápidamente pestañas del menú
         }
+
         private void filtrarTabla()
         {
             if (string.IsNullOrEmpty(txtFiltrar.Text))
             {
-               // vwjugadorBindingSource.Filter = string.Empty;
+                vwequipoBindingSource.Filter = string.Empty;
             }
             else
             {
-              //  vwjugadorBindingSource.Filter = string.Format("{0}='{1}'", cboBuscar.Text, txtFiltrar.Text);
+                vwequipoBindingSource.Filter = string.Format("{0}='{1}'", cboBuscar.Text, txtFiltrar.Text);
             }
         }
+
+        public void actualizarTablaJugadores()
+        {
+            this.vwequipoTableAdapter.Fill(this.vwEquipo.vwequipo);
         }
+    }
 }

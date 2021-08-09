62bddc89cf2e48f18dcabbd58379c27cf6a98524
Jorge Gonzalez <jegnzc@gmail.com>
2021-08-09 15:11:44 -0600

    Mejoras a la vista de roles

---
+++ Polideportivo/Modelo/DAO/daoRol.cs
@@ -0,0 +1,106 @@
+﻿using Dapper;
+using Conexion;
+using Modelo;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using Modelo.DTO;
+
+namespace Modelo.DAO
+{
+    public class daoRol
+    {
+        private ConexionODBC ConexionODBC = new ConexionODBC();
+
+        public daoRol()
+        {
+        }
+
+        public dtoRol agregarRol(dtoRol modelo)
+        {
+            OdbcConnection conexionODBC = ConexionODBC.abrirConexion();
+            if (conexionODBC != null)
+            {
+                var sqlinsertar =
+               "INSERT INTO rol (nombre, fkIdDeporte) " +
+               "VALUES (?nombre?, ?fkIdDeporte?);";
+                var ValorDeVariables = new
+                {
+                    nombre = modelo.nombre,
+                    fkIdDeporte = modelo.fkIdDeporte
+                };
+                conexionODBC.Execute(sqlinsertar, ValorDeVariables);
+                ConexionODBC.cerrarConexion(conexionODBC);
+            }
+            return modelo;
+        }
+
+        public dtoRol modificarRol(dtoRol modelo)
+        {
+            OdbcConnection conexionODBC = ConexionODBC.abrirConexion();
+            if (conexionODBC != null)
+            {
+                var sqlinsertar =
+                "UPDATE rol SET nombre = ?nombre?, " +
+                "fkIdDeporte = ?fkIdDeporte?" +
+                " WHERE pkId = ?pkId?;";
+                var ValorDeVariables = new
+                {
+                    nombre = modelo.nombre,
+                    fkIdDeporte = modelo.fkIdDeporte,
+                    pkId = modelo.pkId
+                };
+                conexionODBC.Execute(sqlinsertar, ValorDeVariables);
+                ConexionODBC.cerrarConexion(conexionODBC);
+            }
+            return modelo;
+        }
+
+        public List<dtoRol> mostrarRoles()
+        {
+            OdbcConnection conexionODBC = ConexionODBC.abrirConexion();
+            List<dtoRol> sqlresultado = new List<dtoRol>();
+            if (conexionODBC != null)
+            {
+                string sqlconsulta = "SELECT * FROM tablaRol;";
+                sqlresultado = conexionODBC.Query<dtoRol>(sqlconsulta).ToList();
+                ConexionODBC.cerrarConexion(conexionODBC);
+            }
+            return sqlresultado;
+        }
+
+        public dtoRol eliminarRol(dtoRol modelo)
+        {
+            OdbcConnection conexionODBC = ConexionODBC.abrirConexion();
+            if (conexionODBC != null)
+            {
+                var sqlinsertar =
+                "DELETE FROM rol WHERE pkId = ?pkId?;";
+                var ValorDeVariables = new
+                {
+                    pkId = modelo.pkId
+                };
+                conexionODBC.Execute(sqlinsertar, ValorDeVariables);
+                ConexionODBC.cerrarConexion(conexionODBC);
+            }
+            return modelo;
+        }
+
+        public List<dtoRol> mostrarRolesPorDeporte(dtoRol modelo)
+        {
+            OdbcConnection conexionODBC = ConexionODBC.abrirConexion();
+            List<dtoRol> sqlresultado = new List<dtoRol>();
+            if (conexionODBC != null)
+            {
+                string sqlconsulta = "SELECT pkId, nombre, fkIdDeporte FROM rol WHERE fkIdDeporte = ?fkIdDeporte?;";
+                var parameters = new
+                {
+                    fkIdDeporte = modelo.fkIdDeporte
+                };
+                sqlresultado = conexionODBC.Query<dtoRol>(sqlconsulta, parameters).ToList();
+                ConexionODBC.cerrarConexion(conexionODBC);
+            }
+            return sqlresultado;
+        }
+    }
+}
--- Polideportivo/Vista/formRol.cs
+++ Polideportivo/Vista/formRol.cs
@@ -9,116 +9,15 @@ namespace Vista
 {
     public partial class formRol : Form
     {
-        private int id;
-        private string nombre;
-        private int fkIdDeporte;
-
         public formRol()
         {
             InitializeComponent();
-            btnAgregarRol.Visible = true;
-            btnModificarRol.Visible = true;
-            // Llenar combobox de deportes
-            daoDeporte daoDeporte = new daoDeporte();
-            cboDeporte.DataSource = daoDeporte.mostrarDeportes();
-            cboDeporte.DisplayMember = "nombre";
-            cboDeporte.ValueMember = "pkId";
-            cboDeporte.SelectedItem = cboDeporte.Items[0];
-        }
-
-        public void actualizarTablaRol()
-        {
-            this.vwrolTableAdapter.Fill(this.vwRol.vwrol);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            dtoEmpleado modelo = new dtoEmpleado();
-            daoEmpleado db = new daoEmpleado();
-        }
-
-        private modeloRol modeloFila = new modeloRol();
-
-        private void llenarModeloConFilaSeleccionada()
-        {
-            id = stringAInt(tablaRol.SelectedRows[0].Cells[0].Value.ToString());
-            nombre = tablaRol.SelectedRows[0].Cells[1].Value.ToString();
-            modeloFila.pkId = id;
-        }
-
-        private void tablaRol_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            llenarModeloConFilaSeleccionada();
-            txtNombreRol.Text = nombre;
-        }
-
-        private void btnAgregarRol_Click(object sender, EventArgs e)
-        {
-            controladorRol modeloAgregar = new controladorRol();
-            modeloRol modelo = new modeloRol();
-            modelo.nombre = txtNombreRol.Text;
-            modelo.fkIdDeporte = stringAInt(cboDeporte.SelectedValue.ToString());
-            modeloAgregar.agregarRol(modelo);
-            actualizarTablaRol();
-        }
-
-        private void btnModificarRol_Click(object sender, EventArgs e)
-        {
-            controladorRol modeloModificar = new controladorRol();
-            modeloRol modelo = new modeloRol();
-            modeloFila.nombre = txtNombreRol.Text;
-            modeloFila.fkIdDeporte = stringAInt(cboDeporte.SelectedValue.ToString());
-            modeloModificar.modificarRol(modeloFila);
-            actualizarTablaRol();
-        }
-
-        private void txtFiltrarRol_TextChanged(object sender, EventArgs e)
-        {
-            if (string.IsNullOrEmpty(txtFiltrarRol.Text))
-            {
-                vwrolBindingSource.Filter = string.Empty;
-            }
-            else
-            {
-                vwrolBindingSource.Filter = string.Format("{0}='{1}'", cboBuscarRol.Text, txtFiltrarRol.Text);
-            }
-        }
-
-        private void cboBuscar_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            txtFiltrarRol.Text = "";
-        }
-
-        private void btnActualizar_Click(object sender, EventArgs e)
-        {
-            actualizarTablaRol();
-        }
-
-        private void btnEliminarRol_Click(object sender, EventArgs e)
-        {
-            int id = stringAInt(tablaRol.SelectedRows[0].Cells[0].Value.ToString());
-            controladorRol controlador = new controladorRol();
-            modeloRol modelo = new modeloRol();
-            modelo.pkId = id;
-            controlador.eliminarRol(modelo);
-            actualizarTablaRol();
+            controladorRol controladorRol = new controladorRol(this);
         }
 
         private void tablaEmpleado_DataError(object sender, DataGridViewDataErrorEventArgs e)
         {
             // Es necesario para que no den errores cuando se cambia rápidamente pestañas del menú
-        }
-
-        private void tablaRol_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-        }
-
-        private void formRol_Load(object sender, EventArgs e)
-        {
-            // TODO: esta línea de código carga datos en la tabla 'vwRol.vwrol' Puede moverla o quitarla según sea necesario.
-            this.vwrolTableAdapter.Fill(this.vwRol.vwrol);
-            // TODO: esta línea de código carga datos en la tabla 'vwRol.vwrol' Puede moverla o quitarla según sea necesario.
-            this.vwrolTableAdapter.Fill(this.vwRol.vwrol);
         }
     }
 }

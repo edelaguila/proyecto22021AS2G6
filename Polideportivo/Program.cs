c1a7e9f68349b57070bc13cb5081abb2f38db98d
Jorge Gonzalez <jegnzc@gmail.com>
2021-08-10 20:27:32 -0600

    Mejoras al la vista de campeonato

--- Polideportivo/Program.cs
+++ Polideportivo/Program.cs
@@ -14,7 +14,7 @@ namespace Polideportivo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new formUsuario());
+            Application.Run(new formPolideportivo());
         }
     }
 }
